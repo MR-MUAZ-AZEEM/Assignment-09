6e01d5600270c946aade871943786035db7a4b1d
MR-MUAZ-AZEEM <mrmuazazeem7.1@gmail.com>
2025-01-12 12:25:40 -0800

    13 Programs

---
+++ script.js
@@ -0,0 +1,108 @@
+// List of loop programming exercises
+
+// 1. Write a js program to print all natural numbers from 1 to n. - using while loop
+
+// var x = 1;
+// while(x <= 100){
+//     console.log(x);
+//     x++;
+// }
+
+// 2. Write a js program to print all natural numbers in reverse (from n to 1). - using while loop
+
+// var x = 100;
+// while(x >= 1){
+//     console.log(x);
+//     x--;
+// }
+
+// 3. Write a js program to print all alphabets from a to z. - using while loop
+
+// var alph = 65;
+// while(alph <= 90){
+//     console.log(String.fromCharCode(alph));
+//     alph++;
+// }
+
+// // 4. Write a js program to print all even numbers between 1 to 100. - using while loop
+
+// var x = 0;
+// while(x <= 100){
+//     console.log(x);
+//     x = x + 2;
+// }
+
+// 5. Write a js program to print all odd number between 1 to 100.
+ 
+// var x = 1;
+// for(x = 1; x <= 100; x = x + 2){
+//     console.log(x);    
+// }
+
+// 6. Write a js program to find sum of all natural numbers between 1 to n.
+
+// var x = 1;
+// var sum = 0;
+// for(x = 1; x <= 100; x++){
+//     sum = sum + x;
+// }
+// console.log(sum);
+
+// 7. Write a js program to find sum of all even numbers between 1 to n.
+
+// var x = 0;
+// var sum = 0;
+// for(x = 0; x <= 100; x = x + 2){
+//     sum = sum + x;
+// }
+// console.log(sum);
+
+// 8. Write a js program to find sum of all odd numbers between 1 to n.
+
+// var x = 1;
+// var sum = 0;
+// for(x = 1; x <= 100; x = x + 2){
+//     sum = sum + x;
+// }
+// console.log(sum);
+
+// 9. Write a js program to print multiplication table of any number.
+
+// var num = prompt("Enter The Number:");
+// var x = 1;
+// for(x = 1; x <= 10; x++){
+//     console.log(x * num);
+// }
+
+// 10. Write a js program to count number of digits in a number.
+
+// var x = prompt("Enter The Number:");
+// var n = 0;
+// for(n = 0; n = x.length; x++){
+//     console.log(n);    
+// }
+
+// 20. Write a js program to print all ASCII character with their values.
+
+// for(var ac = 32; ac <= 126; ac++){
+//     console.log(String.fromCharCode(ac));    
+// }
+
+// 22. Write a js program to find all factors of a number.
+
+// var x = 100;
+// for(var n = 0; n <= x; n++){
+//     if(x % n == 0){
+//         console.log(n);
+//     }
+// }
+
+// 23. Write a js program to calculate factorial of a number.
+
+// var x = 4;
+// var f = 1;
+// for(var n = 1; n <= x; n++){
+//     f = f * n;
+// }
+// console.log(f);
+
